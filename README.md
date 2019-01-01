4fec28646dbe898d30712a3947643e70fa58f114
Anton Plebanovich <anton.plebanovich@gmail.com>
2019-01-01 03:04:18 +0300

    - carthageSetup.command and carthageSetup.rb to fully setup project for using Carthage - podSetup.command to simplify cocoapods usage - carthageAdd.rb improve - README

---
+++ README.md
@@ -0,0 +1,41 @@
+# Overview
+
+Helpful collection of shell scripts to keep different Macs development environments in sync.
+
+## getXcodeConfig.command
+
+Collects Mac settings by putting setting files into specific folders. Collects Xcode font and color Themes, custom keyboard layouts, Xcode templates, Xcode snippets and Xcode hotkeys.
+
+## setupMac.command
+
+Overrides existing Mac setting files with ones that were saved by `getXcodeConfig.command`.
+
+## updateProject.command
+
+Copies Carthage and Cocoapods scripts into `Scripts/` folder. 
+
+## Finder: YOUR_PROJECT/Scripts/Carthage/carthageSetup.command
+
+Execute `updateProject.command` first. Creates `_Carthage` folder inside `Frameworks` folder with `carthageAdd.command`, `carthageRemove.command` and `carthageUpdate.command` to easily manage Carthage dependencies by just right clicking on a command a select `Open with External Editor`.
+
+Creates `Carthage Install` build phase to keep Carthage dependenciec in sync on each build if it wasn't created yet.
+
+Creates `Carthage Copy` build phase to copy Carthage frameworks if it wasn't created yet. **If your target have this phase with other name - delete your one.**
+
+Sets framework search path for Carthage frameworks if it wasn't set yet.
+
+## Xcode: YOUR_PROJECT/Frameworks/_Carthage/carthageAdd.command
+
+Execute `updateProject.command` first. Just right click on a command a select `Open with External Editor` then follow instructions to add new framework.
+
+## Xcode: YOUR_PROJECT/Frameworks/_Carthage/carthageRemove.command
+
+Execute `updateProject.command` first. Just right click on a command a select `Open with External Editor` then follow instructions to remove existing framework.
+
+## Xcode: YOUR_PROJECT/Frameworks/_Carthage/carthageUpdate.command
+
+Execute `updateProject.command` first. Just right click on a command a select `Open with External Editor` then follow instructions to update existing framework.
+
+## YOUR_PROJECT/Scripts/Cocoapods/podSetup.command
+
+Execute `updateProject.command` first. Tries to update Podfile so after first run of `YOUR_PROJECT/Scripts/Cocoapods/podInstall.command` or `YOUR_PROJECT/Scripts/Cocoapods/podUpdate.command` those files will be attached to Pods project for easy access.
--- Scripts/Carthage/carthageAdd.rb
+++ Scripts/Carthage/carthageAdd.rb
@@ -15,30 +15,45 @@ def addFramework(project, framework_name)
     end
     
     # Adding to Frameworks folder and sorting
-    frameworks_group = project["Frameworks"]
+    frameworks_group = project.frameworks_group
     framework_reference = frameworks_group.new_file framework_path
     frameworks_group.sort
     
     # Enumberate each app target
-    app_targets = ["Elevate", "Elevate Staging", "Elevate Testing"]
+    found_carthage_copy_phase = false
     project.targets.each do |target|
-        if app_targets.include? target.name
-            # Adding framewok to building phase and sort
-            target.frameworks_build_phase.add_file_reference(framework_reference)
-            target.frameworks_build_phase.sort
-            
-            target.build_phases.each do |build_phase|
-                if build_phase.display_name == "Carthage Copy"
-                    # Adding framework to input paths and sort
-                    build_phase.input_paths.push("$(SRCROOT)/" + framework_path)
-                    build_phase.input_paths.sort_by!(&:downcase)
-                    
-                    # Adding framework to output paths and sort
-                    build_phase.output_paths.push("$(BUILT_PRODUCTS_DIR)/$(FRAMEWORKS_FOLDER_PATH)/" + framework_full_name)
-                    build_phase.output_paths.sort_by!(&:downcase)
-                end
+        target.build_phases.each do |build_phase|
+            if build_phase.display_name == "Carthage Copy"
+                # Adding framewok to building phase and sort
+                target.frameworks_build_phase.add_file_reference(framework_reference)
+                target.frameworks_build_phase.sort
+                
+                # Adding framework to input paths and sort
+                build_phase.input_paths.push("$(SRCROOT)/" + framework_path)
+                build_phase.input_paths.sort_by!(&:downcase)
+                
+                # Adding framework to output paths and sort
+                build_phase.output_paths.push("$(BUILT_PRODUCTS_DIR)/$(FRAMEWORKS_FOLDER_PATH)/" + framework_full_name)
+                build_phase.output_paths.sort_by!(&:downcase)
+                
+                found_carthage_copy_phase = true
             end
         end
+    end
+    
+    if !found_carthage_copy_phase
+        abort("\n'Carthage Copy' phase doesn't exist.\n".red)
+    end
+end
+
+def getConfigAttributes(framework_target)
+    default_configuration = framework_target.build_configuration_list[framework_target.build_configuration_list.default_configuration_name]
+    base_configuration_reference = default_configuration.base_configuration_reference
+    if !base_configuration_reference&.real_path.to_s.empty?
+        config = Xcodeproj::Config.new(base_configuration_reference.real_path)
+        config.attributes
+    else
+        return nil
     end
 end
 
@@ -79,40 +94,56 @@ if framework_name.to_s.empty?
     abort("\nFramework name is required\n".red)
 end
 
-framework_full_name = framework_name + ".framework"
-framework_path = "Carthage/Build/iOS/" + framework_full_name
-
-if !File.exist?(framework_path)
-    framework_name = prompt "Can not find '" + framework_name + "' framework. Please enter proper framework name (e.g. Alamofire): "
-    framework_full_name = framework_name + ".framework"
-    framework_path = "Carthage/Build/iOS/" + framework_full_name
-end
-
-if !File.exist?(framework_path)
-    abort("\nFramework doesn't exist. Please check Carthage/Build/iOS/ folder and find the right framework name.\n".red)
-end
-
 project_path = Dir['*.xcodeproj'].first
 project = Xcodeproj::Project.open(project_path)
 
 framework_project_path = Dir['Carthage/Checkouts/' + framework_name + '/*.xcodeproj'].first
+
+if framework_project_path.to_s.empty?
+    abort("\nCan't find framework project\n".red)
+end
+
 framework_shared_schemes = Xcodeproj::Project.schemes(framework_project_path)
 
 # Get proper targets
 framework_project = Xcodeproj::Project.open(framework_project_path)
 framework_targets = framework_project.native_targets.select { |framework_target|
-    isIOS = framework_target.common_resolved_build_setting("SUPPORTED_PLATFORMS").include?("iphoneos")
+    isIOS = framework_target.common_resolved_build_setting("SUPPORTED_PLATFORMS")&.include?("iphoneos") || framework_target.platform_name == :ios
+    
+    # Check target configuration file also
+    isIOS = isIOS || getConfigAttributes(framework_target)&.dig("SUPPORTED_PLATFORMS")&.include?("iphoneos")
+    
     isFramework = framework_target.symbol_type == :framework
     isShared = framework_shared_schemes.include?(framework_target.name)
+    
     isIOS && isFramework && isShared
 }
 
-all_framework_names = framework_targets.map { |framework_target| framework_target.common_resolved_build_setting("PRODUCT_NAME") }
+all_framework_names = framework_targets.map { |framework_target|
+    
+    # Check target configuration file first
+    framework_name = getConfigAttributes(framework_target)&.dig("PRODUCT_NAME")&.delete! ';'
+    
+    if framework_name.to_s.empty?
+        framework_name = framework_target.common_resolved_build_setting("PRODUCT_NAME")
+    end
+    
+    if framework_name == '$(TARGET_NAME)'
+        framework_name = framework_target.name
+    end
+    
+    framework_name
+}
 # TODO: Add support for 'c99extidentifier' if needed
 
-if all_framework_names.count == 1
+
+if all_framework_names.empty?
+    abort("\nFramework wasn't found\n".red)
+    
+elsif all_framework_names.count == 1
     addFramework(project, all_framework_names.first)
-    else
+    
+else
     print "\n"
     print "Available frameworks:\n"
     print all_framework_names.join("\n").blue
