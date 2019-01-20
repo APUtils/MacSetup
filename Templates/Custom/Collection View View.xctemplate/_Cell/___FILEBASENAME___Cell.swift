75cd759983d6ad120ecdaf559bc6ff8e68291c1f
Anton Plebanovich <anton.plebanovich@gmail.com>
2019-01-20 15:30:11 +0300

    - Collection View View - Collection View icons

---
+++ Templates/Custom/Collection View View.xctemplate/_Cell/___FILEBASENAME___Cell.swift
@@ -0,0 +1,51 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by Anton Plebanovich on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+
+import APExtensions
+import BaseClasses
+
+
+final class ___FILEBASENAME___: CollectionViewCell {
+    
+    // ******************************* MARK: - @IBOutlets
+    
+    @IBOutlet private weak var <#name#>: <#class#>!
+    @IBOutlet private weak var <#name#>: <#class#>!
+    @IBOutlet private weak var <#name#>: <#class#>!
+    @IBOutlet private weak var <#name#>: <#class#>!
+    @IBOutlet private weak var <#name#>: <#class#>!
+    
+    // ******************************* MARK: - Private Properties
+    
+    private let vm: ___VARIABLE_BASENAME___CellViewModel!
+    
+    // ******************************* MARK: - Initialization and Setup
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setup()
+    }
+    
+    private func setup() {
+        
+    }
+    
+    // ******************************* MARK: - Configuration
+    
+    func configure(vm: ___VARIABLE_BASENAME___CellViewModel) {
+        self.vm = vm
+        
+        <#name#>.<#text#> = vm.<#title#>
+        <#name#>.<#text#> = vm.<#title#>
+        <#name#>.<#text#> = vm.<#title#>
+        <#name#>.<#text#> = vm.<#title#>
+        <#name#>.<#text#> = vm.<#title#>
+    }
+}
---
+++ Templates/Custom/Collection View View.xctemplate/___FILEBASENAME___View.swift
@@ -0,0 +1,74 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by Anton Plebanovich on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+
+import APExtensions
+
+
+final class ___FILEBASENAME___: UIView {
+    
+    // ******************************* MARK: - @IBOutlets
+    
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
+    // ******************************* MARK: - Private Properties
+    
+    private var vm: ___VARIABLE_BASENAME___ViewModel!
+    
+    // ******************************* MARK: - Initialization and Setup
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setup()
+    }
+    
+    private func setup() {
+        setupContentView()
+        setupCollectionView()
+    }
+    
+    private func setupContentView() {
+        let contentView = createContentView()
+        contentView.frame = bounds
+        addSubview(contentView)
+    }
+    
+    private func setupCollectionView() {
+        collectionView.registerNib(cellClass: ___VARIABLE_BASENAME___Cell.self)
+    }
+    
+    // ******************************* MARK: - UIView Overrides
+    
+    // ******************************* MARK: - Configuration
+    
+    func configure(vm: ___VARIABLE_BASENAME___ViewModel) {
+        self.vm = vm
+        collectionView.reloadData()
+    }
+    
+    // ******************************* MARK: - Private Methods
+}
+
+// ******************************* MARK: - InstantiatableContentView
+
+extension ___FILEBASENAME___: InstantiatableContentView {}
+
+// ******************************* MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+
+extension ___FILEBASENAME___: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return vm.cellVMs.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: ___VARIABLE_BASENAME___Cell = collectionView.dequeueCell(for: indexPath)
+        cell.configure(vm: vm.cellVMs[indexPath.row])
+        return cell
+    }
+}
