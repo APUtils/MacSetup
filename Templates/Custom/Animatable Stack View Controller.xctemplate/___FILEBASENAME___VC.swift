f29e1b1990206bfd552216788fc07f2d8039a85f
Anton Plebanovich <anton.plebanovich@gmail.com>
2021-03-15 10:02:22 +0300

    - Animatable stack view controller template

---
+++ Templates/Custom/Animatable Stack View Controller.xctemplate/___FILEBASENAME___VC.swift
@@ -0,0 +1,50 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by Anton Plebanovich on ___DATE___.
+//  Copyright © ___YEAR___ Anton Plebanovich. All rights reserved.
+//
+
+import AnimatableStackView
+import APExtensions
+import APPropertyWrappers
+import UIKit
+
+final class ___FILEBASENAME___: UIViewController {
+    
+    // ******************************* MARK: - @IBOutlets
+    
+    @IBOutlet private var stackView: AnimatableStackView!
+    
+    // ******************************* MARK: - Private Properties
+    
+    @LateInitialized private var vm: ___VARIABLE_BASENAME___VM
+    
+    // ******************************* MARK: - Initialization and Setup
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() {
+        
+    }
+    
+    // ******************************* MARK: - UIViewController Overrides
+    
+    // ******************************* MARK: - Actions
+    
+    // ******************************* MARK: - Notifications
+}
+
+// ******************************* MARK: - InstantiatableFromStoryboard
+
+extension ___FILEBASENAME___: InstantiatableFromStoryboard {
+    static func create(vm: ___VARIABLE_BASENAME___VM) -> ___FILEBASENAME___ {
+        let vc = instantiateFromStoryboard()
+        vc.vm = vm
+        return vc
+    }
+}
---
+++ Templates/Custom/Animatable Stack View Controller.xctemplate/___FILEBASENAME___VM.swift
@@ -0,0 +1,21 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by Anton Plebanovich on ___DATE___.
+//  Copyright © ___YEAR___ Anton Plebanovich. All rights reserved.
+//
+
+import APExtensions
+import Foundation
+
+struct ___FILEBASENAME___ {
+    
+    // ******************************* MARK: - Public Properties
+    
+    // ******************************* MARK: - Initialization and Setup
+    
+    init() {
+        
+    }
+}
