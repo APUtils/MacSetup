a4a88a0dffc5f50492517d7c4b66f6e213f9a425
Anton Plebanovich <anton.plebanovich@gmail.com>
2021-10-07 17:46:35 +0300

    - Animatable View Controller setup

---
+++ Templates/Custom/Animatable View Controller.xctemplate/___FILEBASENAME___VC.swift
@@ -0,0 +1,60 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by Anton Plebanovich on ___DATE___.
+//  Copyright © ___YEAR___ Anton Plebanovich. All rights reserved.
+//
+
+import AnimatableStackView
+import APExtensions
+import APPropertyWrappers
+import RxUtils
+import UIKit
+
+final class ___FILEBASENAME___: ViewController {
+    
+    // ******************************* MARK: - @IBOutlets
+    
+    @IBOutlet private var animatableView: AnimatableView!
+    
+    // ******************************* MARK: - Private Properties
+    
+    @LateInitialized private var vm: ___VARIABLE_BASENAME___VM
+    
+    // ******************************* MARK: - Initialization and Setup
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUpdate()
+    }
+    
+    private func setupUpdate() {
+        vm.rx
+            .vms.driveOnNext { [weak self] vms in
+                guard let _self = self else { return }
+                
+                _self.view.animateIfNeeded {
+                    _self.animatableView.update(viewModels: vms)
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    // ******************************* MARK: - UIViewController Overrides
+    
+    // ******************************* MARK: - Actions
+    
+    // ******************************* MARK: - Notifications
+}
+
+// ******************************* MARK: - InstantiatableFromStoryboard
+
+extension ___FILEBASENAME___: InstantiatableFromStoryboard {
+    static func create(vm: ___VARIABLE_BASENAME___VM) -> ___FILEBASENAME___ {
+        let vc = instantiateFromStoryboard()
+        vc.vm = vm
+        return vc
+    }
+}
---
+++ Templates/Custom/Animatable View Controller.xctemplate/___FILEBASENAME___VM.swift
@@ -0,0 +1,42 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by Anton Plebanovich on ___DATE___.
+//  Copyright © ___YEAR___ Anton Plebanovich. All rights reserved.
+//
+
+import AnimatableStackView
+import APExtensions
+import APPropertyWrappers
+import Foundation
+import RxCocoa
+import RxSwift
+
+final class ___FILEBASENAME___: ClassName {
+    
+    // ******************************* MARK: - Properties
+    
+    @BehaviorRelayProjected
+    fileprivate var vms: [AnimatableView_ViewModel] = []
+    
+    // ******************************* MARK: - Initialization and Setup
+    
+    init() {
+        
+    }
+    
+    deinit {
+        logDebug("Deinit \(className)")
+    }
+}
+
+// ******************************* MARK: - Rx
+
+extension PortfolioInfoVM: ReactiveCompatible {}
+
+extension Reactive where Base == PortfolioInfoVM {
+    var vms: Driver<[AnimatableView_ViewModel]> {
+        base.$vms.asDriver()
+    }
+}
